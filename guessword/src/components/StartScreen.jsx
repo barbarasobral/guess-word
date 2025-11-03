180eeb7fe4a99c0de81fc7fd388f121b54785488
Bárbara Thaís Sobral <barbarasobral87@gmail.com>
2025-11-03 18:13:29 -0300

    first commit

---
+++ guessword/src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = ({ startGame }) => {
+  return (
+    <div className="start">
+        <h1>Guess the Word</h1>
+        <p>Clique no botão abaixo para jogar</p>
+        <button onClick={startGame}>Começar o Jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
---
+++ guessword/src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capo", "Pneu", "Antena", "Assento", "Marcha", "Volante"],
+    fruta: ["Banana", "Maca", "Pera", "Mamao", "Laranja", "Uva", "Morango", "Melancia"],
+    corpo: ["Braço", "Perna", "Cerebro", "Pescoço", "Olhos", "Boca", "Nariz", "Orelha"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Java", "Bootstrap"],
+    alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo", "Frango", "Macarrao"],
+};
